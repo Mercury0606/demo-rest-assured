f127bd53dae6ca82041c94d1145278879853b689
Mercury0606 <mercuryzhong0606@gmail.com>
2024-10-21 14:19:39 +0800

    Real Time example to solve Business logic through json response.

--- src/basics/ComplexJsonParse.java
+++ src/basics/ComplexJsonParse.java
@@ -19,13 +19,36 @@ public class ComplexJsonParse {
         String courseTitle1 = jsonPath.getString("courses[1].title");
         System.out.println(courseTitle1);
 
+
+
+
 //        Print all course titles and thier respective prices
         String Title;
         int Price;
+        int Copies;
+        int Sum;
         for (int i = 0; i < count; i++) {
             Title = jsonPath.getString("courses["+i+"].title");
             Price = jsonPath.getInt("courses["+i+"].price");
             System.out.println("The price of the course <" + Title + "> is: " + Price);
         }
+
+
+
+
+
+        System.out.println(">>>>>Print no. of copies sold by RPA Course");
+        for (int i = 0; i < count; i++) {
+            Title = jsonPath.getString("courses["+i+"].title");
+            Copies = jsonPath.getInt("courses["+i+"].copies");
+            if (Title.equalsIgnoreCase("rpa")){
+                System.out.println("The price of the course <" + Title + "> is: " + Copies);
+                break;
+            }
+
+        }
+
+        SumValidation sumValidation = new SumValidation();
+        sumValidation.validate();
     }
 }
---
+++ src/basics/SumValidation.java
@@ -0,0 +1,47 @@
+package basics;
+
+import files.Payload;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+
+public class SumValidation {
+
+    JsonPath jsonPath = new JsonPath(Payload.CoursePrice());
+    int Count = jsonPath.getInt("courses.size()");
+    String Title;
+    int Price;
+    int Copies;
+    int AcutalSum = 0;
+    int ExpectedSum = 0;
+
+    public void SumofCourses() {
+        for (int i = 0; i < Count; i++) {
+
+            Title = jsonPath.getString("courses[" + i + "].title");
+            Price = jsonPath.getInt("courses[" + i + "].price");
+            Copies = jsonPath.getInt("courses[" + i + "].copies");
+            ExpectedSum = Integer.valueOf(jsonPath.getString("dashboard.purchaseAmount"));
+            AcutalSum += Price * Copies;
+        }
+        System.out.println("Actual sum is: " + AcutalSum);
+        System.out.println("expected sum is: " + ExpectedSum);
+    }
+
+    public int getAcutalSum() {
+
+        return AcutalSum;
+    }
+
+    public int getExpectedSum() {
+
+        return ExpectedSum;
+    }
+
+    public void validate(){
+        SumofCourses();
+        int acutalSum = getAcutalSum();
+        int expectedSum = getExpectedSum();
+        Assert.assertEquals(acutalSum,expectedSum);
+    }
+
+}
